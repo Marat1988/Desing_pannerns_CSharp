8543c62e438a72cc7d170a353142a1c309dd528b
Marat <pirat03071988@mail.ru>
2025-11-30 18:57:24 +0200

    Template method pattern

---
+++ Template/Models/AnalogTransmitter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Template.Abstract;
+
+namespace Template.Models
+{
+    public class AnalogTransmitter: Transtimitter
+    {
+        protected override void Modulation() => Console.WriteLine("Модуляция аналогвого сигнала");
+    }
+}
---
+++ Template/Program.cs
@@ -0,0 +1,17 @@
+﻿using Template.Abstract;
+using Template.Models;
+
+namespace Template
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Transtimitter analogTransmitter = new AnalogTransmitter();
+            analogTransmitter.ProcessStart();
+            Console.WriteLine();
+            Transtimitter digitTransmitter = new DigitTransmitter();
+            digitTransmitter.ProcessStart();
+        }
+    }
+}
