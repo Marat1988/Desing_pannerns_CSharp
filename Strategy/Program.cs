441abf1278d53f6a0ce26963b7a4f0598c6276ee
Marat <pirat03071988@mail.ru>
2025-11-30 02:01:10 +0200

    Strategy pattern Example

---
+++ Strategy/Program.cs
@@ -0,0 +1,23 @@
+﻿using Strategy.Model;
+
+namespace Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ResourceReader resourceReader = new ResourceReader(new NewSiteReader());
+
+            string url = "http://news.com";
+            resourceReader.Read(url);
+
+            url = "https://facebook.com";
+            resourceReader.SetStrategy(new SocialNetworkReader());
+            resourceReader.Read(url);
+
+            url = "@news_channel_telegram";
+            resourceReader.SetStrategy(new TelegramChannelReader());
+            resourceReader.Read(url);
+        }
+    }
+}
