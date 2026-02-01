391e5d9913810737870851e5d9b4e04d6713a10c
Marat <pirat03071988@mail.ru>
2026-02-01 16:46:43 +0200

    Рассмотрение паттерна "Абстрактный метод"

---
+++ Порождающие паттерны/FactoryMethod/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod
+{
+    interface IProduction
+    {
+        void Release();
+    }
+
+    class Car : IProduction
+    {
+        public void Release()
+        {
+            Console.WriteLine("Выпущен новый легковой автомобиль");
+        }
+    }
+
+    class Truck : IProduction
+    {
+        public void Release()
+        {
+            Console.WriteLine("Выпущен грузовой автомобиль");
+        }
+    }
+
+    interface IWorkShop
+    {
+        IProduction Create();
+    }
+
+    class CarWorkShop : IWorkShop
+    {
+        public IProduction Create()
+        {
+            return new Car();
+        }
+    }
+
+    class TruckWorkShop : IWorkShop
+    {
+        public IProduction Create()
+        {
+            return new Truck();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IWorkShop creator = new CarWorkShop();
+            IProduction car = creator.Create();
+
+            creator = new TruckWorkShop();
+            IProduction track = creator.Create();
+
+            car.Release();
+            track.Release();
+
+        }
+    }
+}
